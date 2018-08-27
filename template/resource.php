49480ebeaf6fe92b27afe0e468a1585085f58ea1
Roberto Cordeiro Filho <cordeirofilho@gmail.com>
2018-08-27 15:06:55 -0300

    ajax na latera de Artigos Realacionados

--- template/resource.php
+++ template/resource.php
@@ -54,15 +54,9 @@ if ($response){
     // echo "<pre>"; print_r($response_json); echo "</pre>";
     $resource = $response_json->diaServerResponse[0]->match->docs[0];
     $related_docs = $response_json->diaServerResponse[0]->response->docs;
-
     // find similar documents
-    $similar_docs_url = $similar_docs_url . '?adhocSimilarDocs=' . urlencode($resource->reference_title[0]);
-    // get similar docs
-    $similar_docs_xml = @file_get_contents($similar_docs_url);
-    // transform to php array
-    $xml = simplexml_load_string($similar_docs_xml,'SimpleXMLElement',LIBXML_NOCDATA);
-    $json = json_encode($xml);
-    $similar_docs = json_decode($json, TRUE);
+    $similar_docs_url = $similar_docs_url . '?adhocSimilarDocs='.urlencode($resource->reference_title[0]);
+    $similar_query = urlencode($similar_docs_url);
 }
 
 $feed_url = real_site_url($biblio_plugin_slug) . 'biblio-feed?q=' . urlencode($query) . '&filter=' . urlencode($filter);
@@ -195,36 +189,20 @@ $home_url = isset($biblio_config['home_url_' . $lang]) ? $biblio_config['home_ur
                     </div>
                 </section>
                 <aside id="sidebar">
-                    <?php if ( count($similar_docs['document']) > 0 ): ?>
-                        <section class="row-fluid marginbottom25 widget_categories">
-                            <header class="row-fluid border-bottom marginbottom15">
-                                <h1 class="h1-header"><?php _e('Related','biblio'); ?></h1>
-                            </header>
-                            <ul>
-                                <?php foreach ( $similar_docs['document'] as $similar) { ?>
-                                    <li class="cat-item">
-
-                                        <a href="http://pesquisa.bvsalud.org/portal/resource/<?php echo $lang . '/' . $similar['id']; ?>" target="_blank">
-                                            <?php
-                                                $preferred_lang_list = array($lang, 'en', 'es', 'pt');
-                                                // start with more generic title
-                                                $similar_title = is_array($similar['ti']) ? $similar['ti'][0] : $similar['ti'];
-                                                // search for title in different languages
-                                                foreach ($preferred_lang_list as $lang){
-                                                    $field_lang = 'ti_' . $lang;
-                                                    if ($similar[$field_lang]){
-                                                        $similar_title = $similar[$field_lang];
-                                                        break;
-                                                    }
-                                                }
-                                                echo $similar_title;
-                                            ?>
-                                        </a>
-                                    </li>
-                                <?php } ?>
-                            </ul>
-                        </section>
-                    <?php endif; ?>
+                    <section class="row-fluid marginbottom25 widget_categories">
+                        <header class="row-fluid border-bottom marginbottom15">
+                            <h1 class="h1-header"><?php _e('Related','biblio'); ?></h1>
+                        </header>
+                    <ul id="ajax">
+                        
+                    </ul>
+                    </section>
+<?php
+$url=OER_PLUGIN_URL.'template/similar.php?query='.$similar_query.'&lang='.$lang;
+?>
+<script type="text/javascript">
+    show_similar("<?php echo $url; ?>");
+</script>
                 </aside>
                 <div class="spacer"></div>
             </div> <!-- close DIV.detail-area -->
---
+++ template/similar.php
@@ -0,0 +1,35 @@
+
+<?php
+$lang = $_GET['lang'];
+$similar_docs_url = $_GET['query'];
+
+// get similar docs
+$similar_docs_xml = @file_get_contents($similar_docs_url);
+// transform to php array
+$xml = simplexml_load_string($similar_docs_xml,'SimpleXMLElement',LIBXML_NOCDATA);
+$json = json_encode($xml);
+$similar_docs = json_decode($json, TRUE);
+
+foreach ( $similar_docs['document'] as $similar) { ?>
+    <li class="cat-item">
+    <a href="http://pesquisa.bvsalud.org/portal/resource/<?php echo $lang . '/' . $similar['id']; ?>" target="_blank">
+    <?php
+        $preferred_lang_list = array($lang, 'en', 'es', 'pt');
+        // start with more generic title
+        $similar_title = is_array($similar['ti']) ? $similar['ti'][0] : $similar['ti'];
+        // search for title in different languages
+        foreach ($preferred_lang_list as $lang){
+        $field_lang = 'ti_' . $lang;
+        
+        if ($similar[$field_lang]){
+            $similar_title = $similar[$field_lang];
+            break;
+        }
+    }
+    echo $similar_title;
+    ?>
+    </a>
+    </li>
+<?php } ?>
+
+
